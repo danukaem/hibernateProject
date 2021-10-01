fa956b058250c9be49ba13b7a7178fed2bd913f7
danukaem <danukaem@gmail.com>
2021-10-01 23:56:32 +0530

    one to many working 3 - with oneToOne

--- src/main/java/com/suidls/hibernate/repository/LaptopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.suidls.hibernate.repository;
-
-import com.suidls.hibernate.entity.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LaptopRepository extends JpaRepository<Laptop, Integer> {
-}
