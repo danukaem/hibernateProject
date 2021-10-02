b64dc2f17d47b5e9adafe7d4a2496820ceaf5df0
danukaem <danukaem@gmail.com>
2021-10-02 23:32:48 +0530

    @Query annotation

--- src/main/java/com/suidls/hibernate/controller/PassportController.java
+++ src/main/java/com/suidls/hibernate/controller/PassportController.java
@@ -1,6 +1,7 @@
 package com.suidls.hibernate.controller;
 
 import com.suidls.hibernate.entity.PassportDetail;
+import com.suidls.hibernate.repository.PassportRepository;
 import com.suidls.hibernate.service.PassportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,8 @@ public class PassportController {
 
     @Autowired
     PassportService passportService;
+    @Autowired
+    PassportRepository passportRepository;
 
     @PostMapping("/savePassport")
     public PassportDetail savePassportDetails(@RequestBody PassportDetail passportDetail) {
@@ -24,6 +27,11 @@ public class PassportController {
     @GetMapping("/getPassportDetails")
     public List<PassportDetail> getPassportDetails() {
         return passportService.getPassportDetails();
+    }
+
+    @GetMapping("/aaa")
+    public List<PassportDetail> aa() {
+        return passportRepository.getPDFromConty("sri lanka");
     }
 
 }
--- src/main/java/com/suidls/hibernate/repository/PassportRepository.java
+++ src/main/java/com/suidls/hibernate/repository/PassportRepository.java
@@ -2,9 +2,16 @@ package com.suidls.hibernate.repository;
 
 import com.suidls.hibernate.entity.PassportDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface PassportRepository extends JpaRepository<PassportDetail, Integer> {
+
+    @Query(value = "select pd from PassportDetail pd where pd.country= ?1 ")
+    List<PassportDetail> getPDFromConty(String conty);
+
 }
