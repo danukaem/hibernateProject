706103f809b58e0f5f53483f63c7d029874d2bd8
danukaem <danukaem@gmail.com>
2021-10-21 10:43:53 +0530

    minor

--- src/main/java/com/suidls/hibernate/HibernateApplication.java
+++ src/main/java/com/suidls/hibernate/HibernateApplication.java
@@ -8,6 +8,7 @@ public class HibernateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HibernateApplication.class, args);
+        System.out.println("Hello world");
 
     }
 
